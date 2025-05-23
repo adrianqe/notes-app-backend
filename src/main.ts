ba004b4b9e826fecb3891bf35ddbb5a2bff648c3
Adrian Quiros <157768117+adrianqe@users.noreply.github.com>
2025-05-22 21:49:34 -0600

    Update CORS configuration to allow requests from the frontend application

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://notes-app-frontend-26ha.vercel.app'],
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
