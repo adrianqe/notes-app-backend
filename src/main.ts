a8ded072a14ed32f7e4f679b5a527a4152e7b95d
Adrian Quiros <157768117+adrianqe@users.noreply.github.com>
2025-06-06 19:33:11 -0600

    Fix: uncomment CORS configuration in main.ts for enabling specific origins and methods

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  /*app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'https://notes-app-frontend-zeta-two.vercel.app',
-      'https://notes-app-frontend-mvn80doae-adrian-quiros-elizondos-projects.vercel.app',
-      'https://notes-app-frontend-git-main-adrian-quiros-elizondos-projects.vercel.app',
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // solo si usás cookies
-  });*/
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
