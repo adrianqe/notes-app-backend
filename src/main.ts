4a08aa1db30f0c100593a0dd51a6bf9557021ed1
Adrian Quiros <157768117+adrianqe@users.noreply.github.com>
2025-06-06 18:51:39 -0600

    Fix: update CORS configuration to accept multiple frontend origins

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://notes-app-frontend-zeta-two.vercel.app/',
+    origin: [
+      'https://notes-app-frontend-zeta-two.vercel.app/',
+      'https://notes-app-frontend-mvn80doae-adrian-quiros-elizondos-projects.vercel.app/',
+    ],
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
